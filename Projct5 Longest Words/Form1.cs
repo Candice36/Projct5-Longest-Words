a7e64b7606d123acc8147c0e30b27768845cbf08
Candice36 <candicebell@yahoo.com>
2021-07-18 22:00:49 -0500

    Add project files.

---
+++ Projct5 Longest Words/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projct5_Longest_Words
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.MaxLength = 15;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text.Length.ToString();
+
+            if (textBox1.Text.Length == 15)
+            {
+                MessageBox.Show("You entered Max Characters");
+            }
+        }
+    }
+}
---
+++ Projct5 Longest Words/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projct5_Longest_Words
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
